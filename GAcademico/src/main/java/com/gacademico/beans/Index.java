23f5569500fac229cd9753998adb0c5e676f2511
Gilvonaldo Cavalcanti <givonaldoc@gmail.com>
2016-04-10 14:08:25 -0300

    Atualização no pom.xml e inicio do front-end

---
+++ GAcademico/src/main/java/com/gacademico/beans/Index.java
@@ -0,0 +1,17 @@
+package com.gacademico.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * 
+ * @author gilvonaldo
+ *
+ */
+@RequestScoped
+@ManagedBean
+public class Index {
+
+	
+	
+}
