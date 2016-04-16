0f5f2a73977269d54bfd1a2a8eebcecde8cf6b66
Gilvonaldo Cavalcanti <givonaldoc@gmail.com>
2016-04-16 14:43:05 -0300

    Alterações na estrutura do projeto

---
+++ GAcademico/src/main/java/com/gacademico/entities/Grupo.java
@@ -0,0 +1,6 @@
+package com.gacademico.entities;
+
+
+public enum Grupo {
+	ADMIN, ALUNO, PROFESSOR;
+}
---
+++ GAcademico/src/main/java/com/gacademico/util/Messages.java
@@ -0,0 +1,14 @@
+package com.gacademico.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+
+public class Messages {
+	public static void addFlashMessage(String message) {
+        Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+        flash.setKeepMessages(true);
+        flash.setRedirect(true);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
+}
+}
